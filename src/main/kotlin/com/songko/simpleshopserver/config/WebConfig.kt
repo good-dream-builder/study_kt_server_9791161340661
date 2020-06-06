53385c2adb8cbe8df6670c485e79bb2459feefa5
sj.park <sj.park@quantec.co.kr>
2020-06-07 02:02:39 +0900

    application.yml에서만 정보를 수정할 수 있도록 수정

--- src/main/kotlin/com/songko/simpleshopserver/config/WebConfig.kt
+++ src/main/kotlin/com/songko/simpleshopserver/config/WebConfig.kt
@@ -2,6 +2,7 @@ package com.songko.simpleshopserver.config
 
 import com.songko.simpleshopserver.interceptor.TokenValidationInterceptor
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
@@ -12,6 +13,9 @@ class WebConfig
 @Autowired constructor(
         private val tokenValidationInterceptor: TokenValidationInterceptor
 ) : WebMvcConfigurer {
+    @Value("\${simple-shop.file-upload.default-dir}")
+    var resourceLocation: String? = ""
+
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(tokenValidationInterceptor)
                 .addPathPatterns("/api/**")
@@ -19,6 +23,7 @@ class WebConfig
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:///C:/Users/psj/Desktop/simple-shop-datacenter/images")
+                .addResourceLocations(resourceLocation)
+//                .addResourceLocations("file:///C:/Users/psj/Desktop/simple-shop-datacenter/images")
     }
 }
